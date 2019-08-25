8f0cbf1076fbe399798b10ffb123fbe80f25f482
Shivani Garg <shonagarg91@gmail.com>
2019-08-25 15:16:45 +0530

    Ques1 added

---
+++ src/ShivaniG/Sum_Q1.java
@@ -0,0 +1,14 @@
+package ShivaniG;
+
+class Sum_Q1
+{
+	void addition(int x, int y) 
+	{
+		System.out.println("Sum is :"+(x+y));
+	}	
+	public static void main(String[] args) 
+	{
+		Sum_Q1 sum_q1 = new Sum_Q1(); 
+		sum_q1.addition(20,30);
+	}
+}
