2265bea736a8086fe9f681ff0e08ba066b72b265
AmitaRAug19 <amitarout84@gmail.com>
2019-09-08 12:10:11 +0530

    Adding Pattern print P1 & P2

---
+++ src/AmitaR/P2.java
@@ -0,0 +1,23 @@
+package AmitaR;
+
+import java.util.Scanner;
+
+public class P2 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the no of rows");
+		int rows=sc.nextInt();
+				
+		for(int i=1; i<=rows; i++)
+		{
+			for(int j=1; j<=i; j++)		
+			{
+				System.out.print(j+ " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
