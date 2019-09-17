ab8c25d3d7d1e30105bb9d4af7fa525e4fe614e8
AmitaRAug19 <amitarout84@gmail.com>
2019-09-17 08:07:19 +0530

    P10-merge-conflict

---
+++ src/AmitaR/P10.java
@@ -0,0 +1,23 @@
+package AmitaR;
+
+public class P10 {
+
+	public static void main(String[] args) {
+		int num=5;
+		for(int i=1 ;i<num; i++){
+			{
+				for(int j=1; j<=num; j++)
+				{
+					System.out.println(" ");
+				}
+				for(int k=1; k<=i; k++)
+				{
+					System.out.print("*");
+				}
+				System.out.print(" ");
+			}
+			
+		}
+	}
+
+}
